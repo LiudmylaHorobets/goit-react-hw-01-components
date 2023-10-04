aad47f0c228b2aade1006fa3a53f22eaa1879893
LiudmylaHorobets <horobets1323@gmail.com>
2023-10-04 23:07:58 +0300

    created profile

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import user from 'data/user.json';
+import { Profile } from './Profile/Profile.jsx';
+
+// import css from './App.module.css';
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+    </div>
+  );
+};
---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,7 @@
+
+// export const Statistics = () =>
+// {
+//     <div>
+
+//     </div>
+// }
